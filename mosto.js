9eea367b228a73a5d66d305e7b8297aee190dccd
Juan Martin Runge <jmrunge@gmail.com>
2013-03-06 02:01:02 -0300

    Fixed bug of playlists not respecting the order of the files

--- mosto.js
+++ mosto.js
@@ -2,8 +2,9 @@ var melted_node = require('melted-node'),
     melted_xml  = require('node-mlt'),
     fs          = require('fs'),
     yaml        = require('yaml'),
-    sys         = require('sys'),
-    exec        = require('child_process').exec;
+    sys         = require('util'),
+    exec        = require('child_process').exec,
+    Q           = require('q');
     
 function mosto(configFile) {
     var self = this;
@@ -13,7 +14,7 @@ function mosto(configFile) {
         fs.watch(self.config.playlists.base, function(event, filename) {
             self.readPlaylists();
         });
-    }
+    };
     
     mosto.prototype.readConfig = function(callback) {
         console.log("mbc-mosto: [INFO] Reading configuration from " + self.configFile);
@@ -25,7 +26,7 @@ function mosto(configFile) {
     };
 
     mosto.prototype.readPlaylists = function() {
-        console.log("mbc-mosto: [INFO] Start reading playlists from " + self.config.playlists.base)
+        console.log("mbc-mosto: [INFO] Start reading playlists from " + self.config.playlists.base);
         aux = fs.readdirSync(self.config.playlists.base);
         aux.forEach(function(element, index, array){
             var fileContents = fs.readFileSync(self.config.playlists.base + "/" + element);
@@ -40,11 +41,11 @@ function mosto(configFile) {
                 console.log(playlist);
                 self.playlists.push(playlist);
             }
-            self.orderPlaylist();
+            self.orderPlaylists();
         });
-    }
+    };
     
-    mosto.prototype.orderPlaylist = function() {
+    mosto.prototype.orderPlaylists = function() {
         console.log("mbc-mosto: [INFO] Start ordering playlists:");
         console.log(self.playlists);
         self.playlists.sort(function (item1, item2) {
@@ -60,10 +61,11 @@ function mosto(configFile) {
         self.playlists.forEach(function(element, index, array){
            self.playPlaylist(element); 
         });
-    }
+    };
 
     mosto.prototype.playPlaylist = function(playlist) {
         console.log("mbc-mosto: [INFO] Preparing xml for playlist " + playlist.name);
+        var xmlFiles = [];
         playlist.files.forEach(function(element, index, array){
             var xml = new melted_xml();
             
@@ -82,44 +84,75 @@ function mosto(configFile) {
             var video = new melted_xml.Producer.Video({source: file});
             xml.push(video);
             
+            console.log("mbc-mosto: [INFO] Creating playlist xml object for file " + file);
             var pl = new melted_xml.Playlist;
             pl.entry({producer: video});
             xml.push(pl);
 
+            console.log("mbc-mosto: [INFO] Creating track xml object for file " + file);
             var track = new melted_xml.Multitrack.Track(pl);
 
+            console.log("mbc-mosto: [INFO] Creating multitrack xml object for file " + file);
             var multitrack = new melted_xml.Multitrack;
             multitrack.addTrack(track);
 
+            console.log("mbc-mosto: [INFO] Creating tractor xml object for file " + file);
             var tractor = new melted_xml.Tractor; 
             tractor.push(multitrack);
             
-            filters.forEach(function(element, index, array){
-                var filter = self.config.filters[element];
-                var filterObj = new melted_xml.Filter[filter.filter.charAt(0).toUpperCase() + filter.filter.slice(1)](filter.properties);
-                console.log("mbc-mosto: [INFO] Adding filter " + filter.filter);
-                console.log(filter.properties);
-                tractor.push(filterObj);
-            });
+//            console.log("mbc-mosto: [INFO] Creating filter xml objects for file " + file);
+//            console.log("mbc-mosto: [INFO] Filters: " + filters);
+//            filters.forEach(function(element, index, array){
+//                var filter = self.config.filters[element];
+//                var filterObj = new melted_xml.Filter[filter.filter.charAt(0).toUpperCase() + filter.filter.slice(1)](filter.properties);
+//                console.log("mbc-mosto: [INFO] Adding filter " + filter.filter);
+//                console.log(filter.properties);
+//                tractor.push(filterObj);
+//            });
 
+            console.log("mbc-mosto: [INFO] Pushing xml for file " + file);
             xml.push(tractor);
             
             var fileName = file.substring(file.lastIndexOf("/") + 1);
             var xmlFile = self.config.playlists.xml + "/" + fileName + ".xml";
+            xmlFiles.push(xmlFile);
             
-            console.log("mbc-mosto: [INFO] Writig file " + xmlFile);
+            console.log("mbc-mosto: [INFO] Writing file " + xmlFile);
             fs.writeFile(xmlFile, xml.toString({pretty:true}), function(err){
                 if (err) {
                     console.error("mbc-mosto: [ERROR] " + err.toString());
                     return;
                 } else {
-                    console.log("mbc-mosto: [INFO] File ready: " + xmlFile + " at " + playlist.startDate)
-                    self.sendXml(xmlFile)
+                    console.log("mbc-mosto: [INFO] File ready: " + xmlFile + " at " + playlist.startDate);
+                    //self.sendXml(xmlFile);
                     //self.scheduleFile(xmlFile, playlist.startDate);
+                    if (index === (array.length - 1))
+                        self.sendFiles(xmlFiles);
                 }
             });
         });
-    }
+    };
+    
+    mosto.prototype.sendFiles = function(xmlFiles) {
+        console.log("mbc-mosto: [INFO] Received files: " + xmlFiles);        
+        var xmlFile = xmlFiles.shift();
+        if (xmlFile !== undefined) {
+            console.log('mbc-mosto: [INFO] Sending file ' + xmlFile);
+            self.mlt.sendCommand("apnd u0 " + xmlFile, "200 OK", function() {
+                self.mlt.sendCommand("play u0", "200 OK", function() {
+                    self.sendFiles(xmlFiles);
+                }, function(error) {
+                    var err = new Error("mbc-mosto: [ERROR] Error playing u0 for file: " + xmlFile + " [" + error + "]");
+                    console.error(err);
+                    throw err;
+                });
+            }, function(error) {
+                var err = new Error("mbc-mosto: [ERROR] Error appending u0 for file: " + xmlFile + " [" + error + "]");
+                console.error(err);
+                throw err;
+            });
+        }
+    };
     
     mosto.prototype.scheduleFile = function(xmlFile, date) {
         var hours   = date.getHours();
@@ -141,32 +174,34 @@ function mosto(configFile) {
             if (error !== null) 
                 console.log("mbc-mosto: [ERROR] Error scheduling file " + xmlFile + " for date " + date.toString() + ": " + error); 
         });
-    }
+    };
     
     mosto.prototype.sendXml = function(xmlFile) {
         console.log('mbc-mosto: [INFO] Sending file ' + xmlFile);
         self.mlt.sendCommand("apnd u0 " + xmlFile, "200 OK");
         self.mlt.sendCommand("play u0", "200 OK");    
-    }
+    };
     
     mosto.prototype.startWatching = function() {
-        console.log("mbc-mosto: [INFO] Start watching config file " + self.configFile) ;
+        console.log("mbc-mosto: [INFO] Start watching config file " + self.configFile);
         fs.watch(self.configFile, function(event, filename) {
-            if (event == 'rename')
+            if (event === 'rename')
                 throw new Error("mbc-mosto: [ERROR] Config file renaming is not supported");
             self.readConfig();
         });
 
         self.startPlaylistsWatch();
         self.readPlaylists();
-    }
+    };
     
     mosto.prototype.startMeltedServer = function() {
         var result = self.mlt.connect();
         result.then(function() {
             self.startWatching();
         });
-    }
+    };
+    
+    console.log("mbc-mosto: [INFO] Starting mbc-mosto... " + self.configFile) ;
     
     this.configFile = configFile;
     this.config     = false;
@@ -183,5 +218,5 @@ function mosto(configFile) {
 exports = module.exports = function(configFile) {
     var mosto_server = new mosto(configFile);
     return mosto_server;
-}
+};
 var mosto_server = new mosto();
