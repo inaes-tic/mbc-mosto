0f2a7100e74eae1f3cb0705ddd6fa956e6f2fe7c
Tomas Neme <lacrymology@gmail.com>
2013-03-28 16:56:22 -0300

    basic requirements and simple methods
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- drivers/playlists/mongo-driver.js
+++ drivers/playlists/mongo-driver.js
@@ -1,5 +1,34 @@
+var config   = require("./conf/mongo-driver"),
+    Playlist = require('../../api/Playlist'),
+    Media    = require('../../api/Media'),
+    mubsub   = require("mubsub"),
+    moment   = require("moment");
 
 function mongo_driver() {
+    var self = this;
+
+    this.newPlaylistCallback    = undefined;
+    this.updatePlaylistCallback = undefined;
+    this.removePlaylistCallback = undefined;
+
+    console.log("mbc-mosto: [INFO] Creating mongodb playlists driver");
+
+    mongo_driver.prototype.start = function() {
+    };
+    mongo_driver.prototype.registerNewPlaylistListener = function(newPlaylistCallback) {
+        self.newPlaylistCallback = newPlaylistCallback;
+    };
+    mongo_driver.prototype.registerUpdatePlaylistListener = function(updatePlaylistCallback) {
+        self.updatePlaylistCallback = updatePlaylistCallback;
+    };
+    mongo_driver.prototype.registerRemovePlaylistListener = function(removePlaylistCallback) {
+        self.removePlaylistCallback = removePlaylistCallback;
+    };
+    
+    mongo_driver.prototype.getFileName = function(path) {
+        return path.substring(path.lastIndexOf("/"));
+    };
+    
 }
 
 exports = module.exports = function() {
