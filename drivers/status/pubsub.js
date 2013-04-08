031cd4878607c28060a1a2c3f403c393876795e1
Tomas Neme <lacrymology@gmail.com>
2013-04-07 21:21:12 -0300

    start preparing the CaspaDriver object
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- drivers/status/pubsub.js
+++ drivers/status/pubsub.js
@@ -3,20 +3,37 @@
  *
  *******************/
 
-exports = module.exports = function(){
-    var defaults = { // copied from caspa/models.App.Status
-        _id: 0,
-        piece: {
-            previous: null,
-            curent: null,
-            next: null
-        },
-        show: {
-            previous: null,
-            current: null,
-            next: null,
-        },
-        source: null,
-        no_air: false
-    };
+_ = require('underscore');
+mbc = require('mbc-common');
+
+defaults = { // copied from caspa/models.App.Status
+    _id: 0,
+    piece: {
+        previous: null,
+        curent: null,
+        next: null
+    },
+    show: {
+        previous: null,
+        current: null,
+        next: null,
+    },
+    source: null,
+    no_air: false
 };
+
+function CaspaDriver() {
+    var self = this;
+    this.status = _.clone(defaults);
+    this.channel = "mosto"
+    this.publisher = mbc.JSONredis.createJSONClient();
+    
+    CaspaDriver.prototype.setStatus = function(status) {
+        // this overrides this.status with the values passed by status
+        this.status = _.extend(this.status, status);
+        this.publish(status);
+    };
+    CaspaDriver.prototype.publish = function(status) {
+        this.publisher.publish(this.channel, status);
+    };
+}
