888b04388ec8e5b72b9b653564e8ada7dea79bac
Juan Martin Runge <jmrunge@gmail.com>
2013-04-11 23:12:00 -0300

    Added id, original order, actual order, playlist id and name attrs to Clip
    Signed-off-by: Juan Martin Runge <jmrunge@gmail.com>

--- api/Media.js
+++ api/Media.js
@@ -1,11 +1,16 @@
-function Media(type, file, length, fps) {
-    this.type   = type;
-    this.file   = file;
-    this.length = length;
-    this.fps    = fps;
+function Media(id, orig_order, actual_order, playlist_id, name, type, file, length, fps) {
+    this.id           = id;
+    this.orig_order   = orig_order;
+    this.actual_order = actual_order;
+    this.playlist_id  = playlist_id;
+    this.name         = name;
+    this.type         = type;
+    this.file         = file;
+    this.length       = length;
+    this.fps          = fps;
 }
 
-exports = module.exports = function(type, file, length, fps) {
-    var media = new Media(type, file, length, fps);
+exports = module.exports = function(id, orig_order, actual_order, playlist_id, name, type, file, length, fps) {
+    var media = new Media(id, orig_order, actual_order, playlist_id, name, type, file, length, fps);
     return media;
 };
