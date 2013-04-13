9da32e4272fbf3ea28bf9bdc0094ab828c4b01d9
Niv Sardi <xaiki@evilgiggle.com>
2013-04-13 12:47:37 -0300

    json driver should log as json-driver
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- drivers/playlists/json-driver.js
+++ drivers/playlists/json-driver.js
@@ -10,35 +10,35 @@ function json_driver() {
     this.newPlaylistCallback    = undefined;
     this.updatePlaylistCallback = undefined;
     this.removePlaylistCallback = undefined;
-    
-    console.log("mbc-mosto: [INFO] Creating json playlists driver");
-    
+
+    console.log("json-driver: [INFO] Creating json playlists driver");
+
     json_driver.prototype.start = function() {
-        console.log("mbc-mosto: [INFO] Start watching playlists dir " + config.playlists.to_read) ;
+        console.log("json-driver: [INFO] Start watching playlists dir " + config.playlists.to_read) ;
         watchr.watch({
             paths: [config.playlists.to_read],
             listeners: {
                 error: function(err){
-                    console.log("mbc-mosto: [ERROR] Error while watching playlists dir " + config.playlists.to_read, err);
+                    console.log("json-driver: [ERROR] Error while watching playlists dir " + config.playlists.to_read, err);
                 },
                 watching: function(err, watcherInstance, isWatching){
                     if (err) {
-                        console.log("mbc-mosto: [ERROR] Error while watching playlists dir " + config.playlists.to_read, err);
+                        console.log("json-driver: [ERROR] Error while watching playlists dir " + config.playlists.to_read, err);
                     } else {
-                        console.log("mbc-mosto: [INFO] Finish watching playlists dir " + config.playlists.to_read);
+                        console.log("json-driver: [INFO] Finish watching playlists dir " + config.playlists.to_read);
                     }
                 },
                 change: function(changeType, filePath, fileCurrentStat, filePreviousStat){
                     var name = self.getFileName(filePath);
                     
                     if (changeType === "create") {
-                        console.log("mbc-mosto: [INFO] Playlist added: " + name);
+                        console.log("json-driver: [INFO] Playlist added: " + name);
                         self.createPlaylist(config.playlists.to_read, name, self.newPlaylistCallback);                    
                     } else if (changeType === "update") {
-                        console.log("mbc-mosto: [INFO] Playlist updated: " + name);
+                        console.log("json-driver: [INFO] Playlist updated: " + name);
                         self.createPlaylist(config.playlists.to_read, name, self.updatePlaylistCallback);                    
                     } else if (changeType === "delete") {
-                        console.log("mbc-mosto: [INFO] Playlist deleted: " + name);
+                        console.log("json-driver: [INFO] Playlist deleted: " + name);
                         self.removePlaylistCallback(name);
                     }
                 }
@@ -62,7 +62,7 @@ function json_driver() {
     };
     
     json_driver.prototype.readPlaylists =  function() {
-        console.log("mbc-mosto: [INFO] Start reading playlists from " + config.playlists.to_read);
+        console.log("json-driver: [INFO] Start reading playlists from " + config.playlists.to_read);
         var aux = fs.readdirSync(config.playlists.to_read);
         aux.forEach(function(element, index, array){
             self.createPlaylist(config.playlists.to_read, element, self.newPlaylistCallback);
@@ -70,10 +70,10 @@ function json_driver() {
     };
     
     json_driver.prototype.createPlaylist = function(dir, name, callback) {
-        console.log("mbc-mosto: [INFO] Reading playlist: " + name);
+        console.log("json-driver: [INFO] Reading playlist: " + name);
         var file = fs.readFileSync(dir + "/" + name);
         var aux = JSON.parse(file);
-        console.log("mbc-mosto: [INFO] Parsing playlist:");
+        console.log("json-driver: [INFO] Parsing playlist:");
         console.log(aux);
 
         var split = aux.startDate.split(" ");
@@ -107,11 +107,11 @@ function json_driver() {
                 type = element.type;
             }
             if (file === undefined) {
-                console.warn("mbc-mosto: [WARNING] Cant add a media without the [file] attribute, skipping...");
+                console.warn("json-driver: [WARNING] Cant add a media without the [file] attribute, skipping...");
             } else if (length === undefined) {
-                console.warn("mbc-mosto: [WARNING] Cant add media [" + file + "] without the [length] attribute, skipping...");
+                console.warn("json-driver: [WARNING] Cant add media [" + file + "] without the [length] attribute, skipping...");
             } else if (length === undefined) {
-                console.warn("mbc-mosto: [WARNING] Cant add media [" + file + "] without the [fps] attribute, skipping...");
+                console.warn("json-driver: [WARNING] Cant add media [" + file + "] without the [fps] attribute, skipping...");
             } else {
                 medias.push(new Media(type, file, length, parseFloat(fps)));
                 var clipLength = length.split(":");
