fbcf0b31e478df7e68a98ca2930c8c0c0989ebe9
Niv Sardi <xaiki@evilgiggle.com>
2013-04-13 15:38:19 -0300

    whitespaces
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- drivers/playlists/mongo-driver.js
+++ drivers/playlists/mongo-driver.js
@@ -106,7 +106,7 @@ function mongo_driver(conf) {
             }
             return ret;
         } else {
-            
+
             var window = {
                 from: moment(from),
                 to: moment(to),
