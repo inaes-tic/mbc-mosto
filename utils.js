d5cc95db59d75b64b438531b13017e5243651c8d
Juan Martin Runge <jmrunge@gmail.com>
2013-04-16 22:38:24 -0300

    Added getTimeLengthFromFrames and getCurrentPosFromClip functions to utils
    Signed-off-by: Juan Martin Runge <jmrunge@gmail.com>

--- utils.js
+++ utils.js
@@ -11,4 +11,23 @@ exports = module.exports = {
         var aux = filename.split("-")[1];
         return aux.substring(0, aux.length - 5);
     },
+            
+    getTimeLengthFromFrames: function(frames, fps) {
+        var seconds = parseFloat(frames) / parseFloat(fps);
+        var minutes = 0;
+        if (seconds > 60) {
+            minutes = parseInt(seconds / 60);
+            seconds = parseInt (seconds - (minutes * 60));
+        }
+        var hours = 0;
+        if (minutes > 60) {
+            hours = parseInt(hours / 60);
+            minutes = parseInt(minutes - (hours * 60));
+        }
+        return "." + hours + ":" + minutes + ":" + seconds;
+    },
+    
+    getCurrentPosFromClip: function(actualFrame, totalFrames) {
+        return parseFloat(actualFrame / totalFrames);
+    }
 };
