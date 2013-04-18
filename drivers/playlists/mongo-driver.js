c3a49e68eba66a2688b9eb0f98970e2ddd0932be
fabriciocosta <fabricio.costa@moldeointeractive.com.ar>
2013-04-18 11:55:11 -0300

    Adding "snap" default mode when building Playlist createPlaylist
    Signed-off-by: fabriciocosta <fabricio.costa@moldeointeractive.com.ar>

--- drivers/playlists/mongo-driver.js
+++ drivers/playlists/mongo-driver.js
@@ -200,7 +200,7 @@ function mongo_driver(conf) {
                 medias.push(new Media(block_id, orig_order, actual_order, playlist_id, clip_name, type, file, length, parseFloat(fps)));
             });
 
-            var playlist = new Playlist(playlist_id, name, startDate, medias, endDate);
+            var playlist = new Playlist(playlist_id, name, startDate, medias, endDate, "snap");
 
             if( callback )
                 callback(err, playlist);
