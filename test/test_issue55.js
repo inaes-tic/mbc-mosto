668a7f72664b5df9809b95a31a7f5f9f846b4330
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2013-04-22 13:49:36 -0300

    Test are working in async mode using semaphores.

--- test/test_issue55.js
+++ test/test_issue55.js
@@ -2,36 +2,37 @@ var assert = require("assert");
 var melted = require('../api/Melted');
 
 describe('Melted', function(){
-
-	describe('#semaphore', function(){
-		var serie = ""
-		melted.take(function() {
-			serie = serie + ["1"];
-			setTimeout(function(){serie = serie + "1"; melted.leave();},1000);
-		});
-		melted.take(function(leave, start, stop, get_setup, setup) {
-			serie = serie + ["2"];
-			setTimeout(function(){serie = serie + "2"; melted.leave();},1);
-		});
-		melted.take(function() {
-			serie = serie + ["3"];
-			setTimeout(function(){serie = serie + "3"; melted.leave();},100);
-		});
-		melted.take(function() {
-			it('Async semaphore checking', function() {
-				assert.equal(serie == "112233");
+	describe('#take - #leave', function(){
+		var serie = "";
+		it('Async semaphore checking', function(done) {
+			melted.take(function() {
+				serie = serie + ["1"];
+				setTimeout(function(){serie = serie + "1"; melted.leave();},50);
 			});
-			melted.leave();
+			melted.take(function(leave, start, stop, get_setup, setup) {
+				serie = serie + ["2"];
+				setTimeout(function(){serie = serie + "2"; melted.leave();},1);
+			});
+			melted.take(function() {
+				serie = serie + ["3"];
+				setTimeout(function(){serie = serie + "3"; melted.leave();},10);
+			});
+			melted.take(function() {
+				assert.equal(serie, "112233");
+				melted.leave();
+				done();
+			});
 		});
 	});
 
 	describe('#start', function(){
-		melted.take(function() {
-			melted.start(function(pid) {
-				melted.is_running(function(is) {
-					it('melted should be started', function(){
+		it('melted should be started', function(done){
+			melted.take(function() {
+				melted.start(function(pid) {
+					melted.is_running(function(is) {
 						assert.equal(true, is);
 						melted.leave();
+						done();
 					})
 				});
 			});
@@ -39,41 +40,39 @@ describe('Melted', function(){
 	});
 
 	describe('#stop', function(){
-		melted.take(function() {
-			melted.stop(function(pid) {
-				melted.is_running(function(is) {
-					it('melted should be stopped', function(){
+		it('melted should be stopped', function(done){
+			melted.take(function() {
+				melted.stop(function(pid) {
+					melted.is_running(function(is) {
 						assert.equal(false, is);
 						melted.leave();
+						done();
 					})
 				});
 			});
 		});
 	});
 
-	describe('#change setup', function(){
-		melted.take(function() {
-			melted.update_config({root: '/home/'});
-			it('new configuration ok', function(){
-				assert.equal(melted.get_config().root, "/home/");
-				melted.leave();
-			});
+	describe('#get_config - #update_config - #setup', function(){
+		it('original configuration ok', function(){
+			assert.notEqual(melted.get_config().root, "/home/");
 		});
-	});
-
-	describe('#setup', function(){
-		melted.take(function() {
-			melted.stop(function(pid) {
+		it('new configuration ok', function(){
+			melted.update_config({root: '/home/'});
+			assert.equal(melted.get_config().root, "/home/");
+		});
+		it('setup happens', function(done){
+			melted.take(function() {
 				melted.start(function(pid) {
 					melted.setup(undefined, undefined, function(had_err) {
-						it('setup happens', function(){
-							assert.equal(had_err, false);
-							melted.leave();
-						});
+						assert.equal(had_err, false);
+						melted.leave();
+						done();
 					});
 				});
 			});
 		});
 	});
-})
+
+});
 
