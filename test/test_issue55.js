b887740154955f23a637af9c8c2b912d18cd35c3
Cristian Sebastian Rocha <cristian.rocha@moldeo.coop>
2013-04-22 12:34:36 -0300

    Use take and leave to control access to melted in tests.

--- test/test_issue55.js
+++ test/test_issue55.js
@@ -1,13 +1,79 @@
-var melted = require('../api/Melted')
+var assert = require("assert");
+var melted = require('../api/Melted');
 
-melted.start();
+describe('Melted', function(){
 
-console.info("Melted is working:", melted.is_running());
+	describe('#semaphore', function(){
+		var serie = ""
+		melted.take(function() {
+			serie = serie + ["1"];
+			setTimeout(function(){serie = serie + "1"; melted.leave();},1000);
+		});
+		melted.take(function(leave, start, stop, get_setup, setup) {
+			serie = serie + ["2"];
+			setTimeout(function(){serie = serie + "2"; melted.leave();},1);
+		});
+		melted.take(function() {
+			serie = serie + ["3"];
+			setTimeout(function(){serie = serie + "3"; melted.leave();},100);
+		});
+		melted.take(function() {
+			it('Async semaphore checking', function() {
+				assert.equal(serie == "112233");
+			});
+			melted.leave();
+		});
+	});
 
-melted.stop();
+	describe('#start', function(){
+		melted.take(function() {
+			melted.start(function(pid) {
+				melted.is_running(function(is) {
+					it('melted should be started', function(){
+						assert.equal(true, is);
+						melted.leave();
+					})
+				});
+			});
+		});
+	});
 
-console.info("Melted is working:", melted.is_running());
+	describe('#stop', function(){
+		melted.take(function() {
+			melted.stop(function(pid) {
+				melted.is_running(function(is) {
+					it('melted should be stopped', function(){
+						assert.equal(false, is);
+						melted.leave();
+					})
+				});
+			});
+		});
+	});
 
-setTimeout(function() {
-	console.info("Melted is working:", melted.isRunning());
-	}, 1500);
+	describe('#change setup', function(){
+		melted.take(function() {
+			melted.update_config({root: '/home/'});
+			it('new configuration ok', function(){
+				assert.equal(melted.get_config().root, "/home/");
+				melted.leave();
+			});
+		});
+	});
+
+	describe('#setup', function(){
+		melted.take(function() {
+			melted.stop(function(pid) {
+				melted.start(function(pid) {
+					melted.setup(undefined, undefined, function(had_err) {
+						it('setup happens', function(){
+							assert.equal(had_err, false);
+							melted.leave();
+						});
+					});
+				});
+			});
+		});
+	});
+})
+
