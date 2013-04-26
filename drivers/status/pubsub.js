417d5edc96d0ee669abb0fee645693c95bf7f2e8
Tomas Neme <lacrymology@gmail.com>
2013-04-26 16:49:19 -0300

    move prototype definitions out of constructor function
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- drivers/status/pubsub.js
+++ drivers/status/pubsub.js
@@ -38,97 +38,101 @@ function CaspaDriver() {
     this.status = _.clone(defaults);
     this.db = mbc.db();
     this.publisher = mbc.pubsub();
-
-    CaspaDriver.prototype.setupAll = function() {
-        var setups = [
-            this.setupStatus.bind(this),
-            this.setupMessages.bind(this),
-        ];
-        var sendReady = _.after(setups.length, function() {
-            self.emit('ready');
-        });
-        setups.forEach(function(setup){
-            setup(sendReady);
-        });
-    };
-
-    CaspaDriver.prototype.setupStatus = function(callback) {
-        var col = this.db.collection('status');
-        col.findOne({_id: 2}, function(err, res) {
-            if( err )
-                // err.. do something?
-                return;
-            if( !res ) {
-                // the status doesn't exist, create it
-                col.save(self.status, function(err, itm) {
-                    callback();
-                });
-            } else {
-                // res existed, just signal as ready
-                callback();
-            }
-        });
-    };
-
-    CaspaDriver.prototype.setupMessages = function(callback) {
-        // I think we should assume at init there's no sticky errors?
-        this.db.collection('mostomessages').remove(callback);
-    };
-
-    CaspaDriver.prototype.setStatus = function(meltedStatus) {
-        // this overrides this.status with the values passed by status
-        function makePiece(clip) {
-            if (clip===undefined) return { name: '', _id: '' };
-            return {
-                name: clip.name,
-                _id: clip.id
-            }
-        }
-
-        var clips = meltedStatus.clips;
-        var status = {
-            piece: {
-                previous: makePiece(meltedStatus.clip.previous),
-                current:  makePiece(meltedStatus.clip.current),
-                next:     makePiece(meltedStatus.clip.next),
-            },
-            show: {
-                previous: { _id: meltedStatus.show.previous },
-                current: { _id: meltedStatus.show.current },
-                next: { _id: meltedStatus.show.next },
-            },
-            on_air: true,
-        };
-        status.piece.current.progress = meltedStatus.position * 100 + "%";
-
-        this.status = _.extend(this.status, status);
-        // except next. If that's undefined, I just don't know!
-        this.status.show.next = status.show.next;
-        this.publishStatus(status);
-    };
-    CaspaDriver.prototype.publishStatus = function(status) {
-        this.publisher.publishJSON("mostoStatus", status)
-    };
-
-    CaspaDriver.prototype.publishMessage = function(code, description, message, sticky) {
-        message = new MostoMessage(code, description, message);
-        var method = 'emit';
-        if( sticky ) {
-            // I create an id with the timestamp to be able to cancel the error afterwards
-            message.stickId = (new moment()).valueOf();
-            method = 'create';
-        }
-        this.publisher.publishJSON(["mostoMessage", method].join('.'),
-                                   { model: message });
-        return message;
-    };
-
-    CaspaDriver.prototype.dropMessage = function(message) {
-        this.publisher.publish("mostoMessage.delete", { model: message });
-    };
 }
-
 util.inherits(CaspaDriver, events.EventEmitter);
+
+CaspaDriver.prototype.setupAll = function() {
+    var self = this;
+    var setups = [
+        this.setupStatus.bind(this),
+        this.setupMessages.bind(this),
+    ];
+    var sendReady = _.after(setups.length, function() {
+        self.emit('ready');
+    });
+    setups.forEach(function(setup){
+        setup(sendReady);
+    });
+};
+
+CaspaDriver.prototype.setupStatus = function(callback) {
+    var self = this;
+    var col = this.db.collection('status');
+    col.findOne({_id: 2}, function(err, res) {
+        if( err )
+            // err.. do something?
+            return;
+        if( !res ) {
+            // the status doesn't exist, create it
+            col.save(self.status, function(err, itm) {
+                callback();
+            });
+        } else {
+            // res existed, just signal as ready
+            callback();
+        }
+    });
+};
+
+CaspaDriver.prototype.setupMessages = function(callback) {
+    // I think we should assume at init there's no sticky errors?
+    this.db.collection('mostomessages').remove(callback);
+};
+
+CaspaDriver.prototype.setStatus = function(meltedStatus) {
+    // this overrides this.status with the values passed by status
+    function makePiece(clip) {
+        if (clip===undefined) return { name: '', _id: '' };
+        return {
+            name: clip.name,
+            _id: clip.id
+        }
+    }
+
+    var clips = meltedStatus.clips;
+    var status = {
+        piece: {
+            previous: makePiece(meltedStatus.clip.previous),
+            current:  makePiece(meltedStatus.clip.current),
+            next:     makePiece(meltedStatus.clip.next),
+        },
+        show: {
+            previous: { _id: meltedStatus.show.previous },
+            current: { _id: meltedStatus.show.current },
+            next: { _id: meltedStatus.show.next },
+        },
+        on_air: true,
+    };
+    status.piece.current.progress = meltedStatus.position * 100 + "%";
+
+    this.status = _.extend(this.status, status);
+    // except next. If that's undefined, I just don't know!
+    this.status.show.next = status.show.next;
+    this.publishStatus(status);
+};
+
+CaspaDriver.prototype.setStatusClip = function(statusClip) {}
+
+CaspaDriver.prototype.publishStatus = function(status) {
+    this.publisher.publishJSON("mostoStatus", status)
+};
+
+CaspaDriver.prototype.publishMessage = function(code, description, message, sticky) {
+    message = new MostoMessage(code, description, message);
+    var method = 'emit';
+    if( sticky ) {
+        // I create an id with the timestamp to be able to cancel the error afterwards
+        message.stickId = (new moment()).valueOf();
+        method = 'create';
+    }
+    this.publisher.publishJSON(["mostoMessage", method].join('.'),
+                               { model: message });
+    return message;
+};
+
+CaspaDriver.prototype.dropMessage = function(message) {
+    this.publisher.publish("mostoMessage.delete", { model: message });
+};
 
 exports = module.exports = function() {
     var driver = new CaspaDriver();
