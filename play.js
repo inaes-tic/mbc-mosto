45ccbc229f4db43a772b7592d6b842e493eda56a
fabriciocosta <fabricio.costa@moldeointeractive.com.ar>
2013-05-10 08:50:03 -0300

    Parsing ints
    Signed-off-by: fabriciocosta <fabricio.costa@moldeointeractive.com.ar>

--- play.js
+++ play.js
@@ -170,10 +170,10 @@ function play( config ) {
         if (    self.actual_playing_status=="playing" 
                 || (self.actual_playing_status=="paused" && self.actual_status.actualClip.id=="black_id") ) {
             self.actual_playing_clip = self.actual_status.actualClip.id;
-            self.actual_playing_index = self.actual_status.actualClip.order;
+            self.actual_playing_index = parseInt(self.actual_status.actualClip.order);
             self.actual_playing_progress = self.actual_status.currentPos;
-            self.actual_playing_frame = self.actual_status.actualClip.currentFrame;
-            self.actual_playing_length = self.actual_status.actualClip.totalFrames;
+            self.actual_playing_frame = parseInt(self.actual_status.actualClip.currentFrame);
+            self.actual_playing_length = parseInt(self.actual_status.actualClip.totalFrames);
             self.actual_playing_fps =  self.actual_status.actualClip.fps;
 
             self.emit('statusclip', self.actual_status.actualClip);
