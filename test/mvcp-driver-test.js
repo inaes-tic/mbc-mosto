c81c37199680328abc563568bd359abe98eaf618
fabriciocosta <fabricio.costa@moldeointeractive.com.ar>
2013-05-13 10:46:05 -0300

    Removing silence log ouput from tests, now it's in 0_console.js
    Signed-off-by: fabriciocosta <fabricio.costa@moldeointeractive.com.ar>

--- test/mvcp-driver-test.js
+++ test/mvcp-driver-test.js
@@ -4,35 +4,6 @@ var mvcp_server = require('../drivers/mvcp/mvcp-driver');
 var Media        = require('../api/Media.js');
 var melted  = require('../api/Melted');
 
-// SILENCE LOG OUTPUT
-var util = require('util');
-var fs = require('fs');
-var log = fs.createWriteStream('./stdout.log');
-
-console.log = console.info = function(t) {
-  var out;
-  if (t && ~t.indexOf('%')) {
-    out = util.format.apply(util, arguments);
-    process.stdout.write(out + '\n');
-    return;
-  } else {
-    out = Array.prototype.join.call(arguments, ' ');
-  }
-  out && log.write(out + '\n');
-};
-// END SILENCE LOG OUTPUT
-
-silence = function(callback) {
-	var ori_console_log = console.log;
-	var ori_console_error = console.error;
-	console.log = function() { };
-	console.error = function() { };
-	var r = callback();
-	console.log = ori_console_log;
-	console.error = ori_console_error;
-	return r;
-}
-
 describe('start mvcp-driver test', function(done) {
 
     var server      = undefined;
