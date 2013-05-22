0a337b4f1876833b1e39016531530c3092d51cee
Tomas Neme <lacrymology@gmail.com>
2013-05-22 18:48:11 -0300

    updatePlaylist
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- fetch-bb.js
+++ fetch-bb.js
@@ -23,6 +23,15 @@ Fetch.prototype.addPlaylist = function(playlist) {
 };
 
 Fetch.prototype.updatePlaylist = function(playlist) {
+    // check if we need to remove it
+    var existing = this.playlists.findWhere({ _id: playlist.get("_id")});
+    if( existing && (playlist.get('start') > this.time_window.end ||
+                     playlist.get('end') < this.time_window.start)) {
+        return this.removePlaylist(playlist.get('_id'));
+    }
+
+    // if we didn't need to remove it, add it. addPlaylist calls with merge: true
+    return this.addPlaylist(playlist);
 };
 
 Fetch.prototype.removePlaylist = function(id) {
