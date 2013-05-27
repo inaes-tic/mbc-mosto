4e672e39f8432e215cd5f81610baf69b821444fa
Tomas Neme <lacrymology@gmail.com>
2013-05-27 18:54:48 -0300

    relational models
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- models/Mosto.js
+++ models/Mosto.js
@@ -9,57 +9,91 @@ var Backbone   = require('backbone')
 
 var Mosto = {};
 
-Mosto.Playlist = Backbone.Model.extend({
-    /**************************
-     * taken from api/Playlist
-     **************************/
-    urlRoot: "mosto/playlist",
-    idAttribute: "_id",
-
-    initialize: function (attributes, options) {
-        console.log ('creating new Mosto.Playlist');
-        Backbone.Model.initialize.call(this, attributes, options);
-        attributes.name = attributes.name || attributes._id;
-
-        if( !attributes.start )
-            throw new Error("Must provide a start date");
-        attributes.start = moment(attributes.start);
-        if (!attributes.end )
-            throw new Error("Must provide an end date");
-        attributes.end = moment(attributes.end);
-
-        if( !medias || !medias.length )
-            throw new Error("Cannot create playlist without medias");
-
-        return attributes;
+Mosto.Media = Backbone.RelationalModel.extend({
+    defaults: {
+        playlist_order: undefined,
+        actual_order: undefined,
+        name: '',
+        type: 'default',
+        file: undefined,
+        length: undefined,
+        fps: undefined,
+        start: undefined,
+        end: undefined,
     },
+});
 
+Mosto.BlankClip = new Mosto.Media({
+    name: path.basename(config.black).replace(path.extname(config.black, '')),
+    file: config.black,
+    fps: config.fps,
+})
 
+Mosto.MediaCollection = Backbone.Collection.extend({
+    /* this is a playlist's list of clips */
+    model: Mosto.Media,
+    comparator: function(model) {
+        return model.get('playlist_order');
     },
+});
+
+Mosto.Playlist = Backbone.RelationalModel.extend({
     defaults: {
         name: null,
         start: null,   // moment
-        medias: [];    // collection?
         end: null,     // moment
         mode: "snap",
         loaded: false,
-    }
+    },
+    relations:[{
+        type: Backbone.HasMany,
+        key: 'medias',
+        relatedModel: Mosto.Media,
+        collectionType: Mosto.MediaCollection,
+        reverseRelation: {
+            key: 'playlist'
+        },
+    }],
+    initialize: function (attributes, options) {
+        console.log ('creating new Mosto.Playlist', attributes, options);
+        Backbone.Model.prototype.initialize.apply(this, attributes, options);
+        this.set('name', this.get('name') || this.get('_id'));
+
+        if( !attributes.start )
+            throw new Error("Must provide a start date");
+        this.set('start', moment(this.get('start')));
+        if (!attributes.end )
+            throw new Error("Must provide an end date");
+        this.set('end', moment(this.get('end')));
+    },
 });
 
-Mosto.Collection = Backbone.Collection.extend({
+Mosto.PlaylistCollection = Backbone.Collection.extend({
     model: Mosto.Playlist,
     comparator: function(playlist) {
         // remember, comparator is called only on insert, update doesn't re-sort
         //  but .add(.., {merge: true}) DOES re-sort!
-        return playlist.get("start"); //XXX(xaiki): should it be _id ?
+        return playlist.get("start");
     },
+    initialize: function() {
+        this.on('sorted', this.addBlanks.bind(this));
+    },
+    sort: function(options) {
+        Backbone.Collection.sort.apply(this);
+    },
+    addBlanks: function(collection, options) {
+    },
+    addBlankPlaylist: function(from, to, at) {
+    },
+});
 
-    url: 'mosto/playlist',
-    backend: 'mostoplaylistbackend',
-    initialize: function () {
-        console.log ('creating new Media.Collection');
-        Backbone.Collection.prototype.initialize.call (this);
-    }
+Mosto.LoadedPlaylists = Backbone.RelationalModel.extend({
+    relations: [{
+        type: Backbone.HasMany,
+        key: 'playlists',
+        relatedModel: Mosto.Playlist,
+        collectionType: Mosto.PlaylistCollection,
+    }],
 });
 
 exports = module.exports = Mosto;
