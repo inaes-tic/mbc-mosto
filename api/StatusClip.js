d01f7e0965fb0932ee51ec4c205be1c695b140a3
Tomas Neme <lacrymology@gmail.com>
2013-06-04 15:38:31 -0300

    make sure the values have the right type
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- api/StatusClip.js
+++ api/StatusClip.js
@@ -1,10 +1,10 @@
 function StatusClip(id, order, playlistId, fps, currentFrame, totalFrames) {
     this.id           = id;
-    this.order        = order;
+    this.order        = parseInt(order);
     this.playlistId   = playlistId;
-    this.fps          = fps;
-    this.currentFrame = currentFrame;
-    this.totalFrames  = totalFrames;
+    this.fps          = parseFloat(fps);
+    this.currentFrame = parseInt(currentFrame);
+    this.totalFrames  = parseInt(totalFrames);
 }
 
 exports = module.exports = function(id, order, playlistId, fps, currentFrame, totalFrames) {
