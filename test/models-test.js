46fba10369759fa1fd6e6ace5f079b0809627899
Tomas Neme <lacrymology@gmail.com>
2013-06-04 20:37:17 -0300

    better test organization
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- test/models-test.js
+++ test/models-test.js
@@ -13,6 +13,19 @@ describe.only('models.Mosto', function() {
     self.medias = helpers.getMedia();
     this.timeout(300000);
 
+    self.createPlaylist = function(medias, start) {
+        var duration = _.reduce(medias, function(acc, m) { return acc + m.get('length') * m.get('fps') }, 0);
+        start = start || moment();
+        var pl = new Mosto.Playlist({
+            id: now.valueOf(),
+            name: 'test',
+            start: moment(now),
+            end: now.add(duration),
+        });
+        pl.get('medias').add(medias);
+        return pl;
+    };
+
     describe('MeltedCollection', function(){
         before(function(done) {
             // restart melted
@@ -35,69 +48,90 @@ describe.only('models.Mosto', function() {
         });
 
         describe("Media juggling", function() {
-            it("should add it's media to melted", function(done) {
-                var mlt_media = self.mlt_media = self.playlists.get('melted_medias');
-                var playlists = self.pls = self.playlists.get('playlists');
-                var medias = self.mediamodels = _.map(self.medias, function(media, ix) { return new Mosto.Media(_.extend(media, { playlist_order: ix })) });
-                var duration = _.reduce(medias, function(acc, media) { return acc + media.get('length') * media.get('fps') * 1000 }, 0);
-                var now = moment();
-                var pl = new Mosto.Playlist({
-                    id: now.valueOf(),
-                    name: 'test',
-                    start: moment(now),
-                    end: now.add(duration),
-                });
-                pl.get('medias').add(medias);
-                self.playlists.addPlaylist(pl);
-                self.playlists.save();
-                mlt_media.take(function() {
-                    mvcp.getServerPlaylist().then(function(clips) {
-                        clips.length.should.eql(self.medias.length);
-                        clips.forEach(function(clip, ix) {
-                            clip.id.should.eql(self.medias[ix].id);
-                        });
-                    }).then(done, done).fin(mlt_media.leave);
-                });
+            before(function() {
+                self.mlt_media = self.playlists.get('melted_medias');
+                self.pls = self.playlists.get('playlists');
+                self.mediamodels = _.map(self.medias, function(media, ix) { return new Mosto.Media(_.extend(media, { playlist_order: ix })) });
             });
-
-            it('A new instance should fetch the playlist from the server', function(done){
-                var mm = new Mosto.MeltedCollection();
-                mm.fetch();
-                mm.take(function() {
-                    mm.length.should.eql(self.medias.length);
-                    done();
-                });
-            });
-
-            describe('remove medias', function() {
-                it('medias removed directly from MeltedCollection should be removed from melted', function(done) {
-                    var collection = self.mlt_media;
-                    collection.remove(self.mediamodels);
-                    collection.take(function() {
-                        mvcp.getServerPlaylist().then(function(clips) {
-                            clips.length.should.eql(0);
-                        }).then(done, done).fin(collection.leave);
+            describe("starting without medias in melted", function(){
+                beforeEach(function(done) {
+                    self.mlt_media.take(function() {
+                        mvcp.cleanPlaylist().then(function(){ done() }, done).fin(self.mlt_media.leave);
                     });
                 });
-                it('but when Playlists.save(), they should be restored', function(done){
+                afterEach(function(done) {
+                    self.pls.set([]);
                     self.playlists.save();
-                    self.mlt_media.take(function(){
+                    self.mlt_media.take(function() {
+                        done();
+                        self.mlt_media.leave()
+                    });
+                });
+                it("should add it's media to melted", function(done) {
+                    var mlt_media = self.mlt_media;
+                    var playlists = self.pls;
+                    var pl = self.createPlaylist(self.mediamodels);
+
+                    self.playlists.addPlaylist(pl);
+                    self.playlists.save();
+                    mlt_media.take(function() {
                         mvcp.getServerPlaylist().then(function(clips) {
                             clips.length.should.eql(self.medias.length);
                             clips.forEach(function(clip, ix) {
                                 clip.id.should.eql(self.medias[ix].id);
                             });
-                        }).then(done, done).fin(self.mlt_media.leave);
+                        }).then(done, done).fin(mlt_media.leave);
                     });
                 });
-                it('removing the Playlist and saving should remove them from melted as well', function(done){
-                    var pl = self.pls.at(0);
-                    self.playlists.removePlaylist(pl);
+            });
+            describe("starting with media in melted", function() {
+                beforeEach(function(done) {
+                    var pl = self.createPlaylist(self.mediamodels);
+                    self.pls.set(pl);
                     self.playlists.save();
                     self.mlt_media.take(function() {
-                        mvcp.getServerPlaylist().then(function(clips) {
-                            clips.length.should.eql(0);
-                        }).then(done, done).fin(self.mlt_media.leave);
+                        done();
+                        self.mlt_media.leave()
+                    });
+                });
+                afterEach(function(done) {
+                    self.pls.set([]);
+                    self.playlists.save();
+                    self.mlt_media.take(function() {
+                        done();
+                        self.mlt_media.leave()
+                    });
+                });
+                it('A new instance should fetch the playlist from the server', function(done){
+                    var mm = new Mosto.MeltedCollection();
+                    mm.fetch();
+                    mm.take(function() {
+                        mm.length.should.eql(self.medias.length);
+                        done();
+                    });
+                });
+
+                describe('remove medias', function() {
+                    it('medias removed directly from MeltedCollection should be removed from melted', function(done) {
+                        var collection = self.mlt_media;
+                        collection.remove(self.mediamodels);
+                        collection.take(function() {
+                            mvcp.getServerPlaylist().then(function(clips) {
+                                clips.length.should.eql(0);
+                            }).then(done, done).fin(collection.leave);
+                        });
+                    });
+                    it('but when Playlists.save(), they should be restored', function(done){
+                        self.playlists.save();
+                        self.mlt_media.take(function(){
+                            mvcp.getServerPlaylist().then(function(clips) {
+                                clips.length.should.eql(self.medias.length);
+                                clips.forEach(function(clip, ix) {
+                                    clip.id.should.eql(self.medias[ix].id);
+                                });
+                            }).then(done, done).fin(self.mlt_media.leave);
+                        });
+                    });
                     });
                 });
             });
