ce0848568658199b40987447e762ab11cdab1e1b
Tomas Neme <lacrymology@gmail.com>
2013-10-09 16:36:43 -0300

    the telltale string changed
    Signed-off-by: Tomas Neme <lacrymology@gmail.com>

--- test/general-functional-test.js
+++ test/general-functional-test.js
@@ -317,7 +317,7 @@ describe("Mosto functional test", function() {
                     promise.then(function(result) {
                         result = result.split('\r\n')[1].split(' ');
                         var file = result[2];
-                        file.should.include('black_id');
+                        file.should.include('BLANK');
                     }).then(done).done();
                 });
             });
