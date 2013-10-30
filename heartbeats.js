f560ec325d4305e4a3e0deaddd3658693a3616a7
Juan Martin Runge <jmrunge@gmail.com>
2013-10-30 09:21:59 -0300

    Fixed window calculation for checkout playlists
    Signed-off-by: Juan Martin Runge <jmrunge@gmail.com>

--- heartbeats.js
+++ heartbeats.js
@@ -111,9 +111,9 @@ heartbeats.prototype.checkSchedules =  function() {
     var cleanMedias = self.melted_medias.where({blank: false});
     var last = cleanMedias[cleanMedias.length - 1];
     if (last) {
-        var scheduled =  last.get('end') - moment();
-        if (scheduled < self.config.min_scheduled) {
-            self.emit("forceCheckout", {from: last.get('end'), to: last.get('end') + scheduled});
+        var to = moment() + self.config.min_scheduled;
+        if (last.get('end') < to) {
+            self.emit("forceCheckout", {from: last.get('end'), to: to});
             logger.debug("Sent forceCheckout event!");
         }
     } else {
