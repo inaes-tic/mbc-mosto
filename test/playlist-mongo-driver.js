dd4a849162028c9f711bd64f035b71d3d271899d
Juan Martin Runge <jmrunge@gmail.com>
2013-11-28 12:08:56 -0300

    Changed the way of creating pieces (using JSON instead of Media.Model) and drop database before test starts
    Signed-off-by: Juan Martin Runge <jmrunge@gmail.com>

--- test/playlist-mongo-driver.js
+++ test/playlist-mongo-driver.js
@@ -26,68 +26,75 @@ describe('PlaylistMongoDriver', function(){
                 self.driver = new mongo_driver(conf);
 
                 self.db = mbc.db(conf.db);
-                self.driver.start();
-                self.from = moment();
-                self.span = 120;
-                self.to = moment((self.from.unix() + self.span * 60) * 1000); // add 2hs
+                self.db.dropDatabase(function(err, success) {
+                    self.driver.start();
+                    self.from = moment();
+                    self.span = 120;
+                    self.to = moment((self.from.unix() + self.span * 60) * 1000); // add 2hs
 
-                self.collections = {
-                    lists: self.db.collection('lists'),
-                    scheds: self.db.collection('scheds'),
-                    pieces: self.db.collection('pieces'),
-                };
-
-                var medias = helpers.getMBCMedia();
-                // let's create a playlist at least 1 hour long
-                var playlist = new Media.Playlist({_id: uuid.v1(), title: 'Name'});
-                while(playlist.get('duration') < 3600000) {
-                    var media = _.randelem(medias);
-                    var piece = new Media.Piece(media.toJSON());
-                    piece.set('_id', uuid.v1());
-                    playlist.get('pieces').add(piece);
-                    playlist.update_duration_nowait(playlist.get('pieces'));
-                }
-                self.pieces = playlist.get('pieces');
-                self.lists = [playlist];
-                // program at least 4hs of schedules
-                self.scheds = [];
-                for(var i = 0 ; i < 5 ; i++) {
-                    var schedule = {
-                        _id: uuid.v1(),
-                        playlist: playlist,
-                        title: playlist.get('title') + i,
+                    self.collections = {
+                        lists: self.db.collection('lists'),
+                        scheds: self.db.collection('scheds'),
+                        pieces: self.db.collection('pieces'),
                     };
-                    var hsix = i - 3;
-                    var now = self.from;
-                    // schedules are from 1hs before now
-                    var schtime = moment(now + (hsix * 30 * 60 * 1000)).valueOf();
-                    var length = moment.duration(playlist.get('duration'));
-                    schedule.start = schtime;
-                    schedule.end = schtime + length;
-                    var occurrence = new Media.Occurrence(schedule);
-                    self.scheds.push(occurrence);
-                };
 
-                var ready = _.after(
-                    self.lists.length + self.pieces.length + self.scheds.length,
-                    function(){ done() });
+                    var medias = helpers.getJSONMedia();
+                    // let's create a playlist at least 1 hour long
+                    var playlist = new Media.Playlist({_id: uuid.v1(), title: 'Name'});
+                    while(playlist.get('duration') < 3600000) {
+                        var m = _.randelem(medias);
+                        var piece = new Media.Piece();
+                        piece.set('_id', uuid.v1());
+                        piece.set('name', m.name);
+                        piece.set('file', m.file);
+                        piece.set('fps', m.fps);
+                        piece.set('durationraw', m.durationraw);
+                        playlist.get('pieces').add(piece);
+                        playlist.update_duration_nowait(playlist.get('pieces'));
+                    }
+                    self.pieces = playlist.get('pieces');
+                    self.lists = [playlist];
+                    // program at least 4hs of schedules
+                    self.scheds = [];
+                    for(var i = 0 ; i < 5 ; i++) {
+                        var schedule = {
+                            _id: uuid.v1(),
+                            playlist: playlist,
+                            title: playlist.get('title') + i,
+                        };
+                        var hsix = i - 3;
+                        var now = self.from;
+                        // schedules are from 1hs before now
+                        var schtime = moment(now + (hsix * 30 * 60 * 1000)).valueOf();
+                        var length = moment.duration(playlist.get('duration'));
+                        schedule.start = schtime;
+                        schedule.end = schtime + length;
+                        var occurrence = new Media.Occurrence(schedule);
+                        self.scheds.push(occurrence);
+                    };
 
-                self.pieces.forEach(function(piece) {
-                    self.collections.pieces.save(piece.toJSON(), {safe:true}, function(err, list) {
-                        ready();
+                    var ready = _.after(
+                        self.lists.length + self.pieces.length + self.scheds.length,
+                        function(){ done() });
+
+                    self.pieces.forEach(function(piece) {
+                        self.collections.pieces.save(piece.toJSON(), {safe:true}, function(err, list) {
+                            ready();
+                        });
                     });
-                });
 
-                self.lists.forEach(function(playlist) {
-                    self.collections.lists.save(playlist.toJSON(), {safe:true}, function(err, list) {
-                        ready();
+                    self.lists.forEach(function(playlist) {
+                        self.collections.lists.save(playlist.toJSON(), {safe:true}, function(err, list) {
+                            ready();
+                        });
                     });
-                });
 
-                self.scheds.forEach(function(occurrence) {
-                    self.collections.scheds.save(occurrence.toJSON(), {safe:true}, function(err, sched){
-                        ready();
+                    self.scheds.forEach(function(occurrence) {
+                        self.collections.scheds.save(occurrence.toJSON(), {safe:true}, function(err, sched){
+                            ready();
+                        });
                     });
+                    
                 });
             });
         });
@@ -128,8 +135,8 @@ describe('PlaylistMongoDriver', function(){
             message.method = 'create';
             self.driver.on('create', function(playlist) {
                 playlist.id.should.be.eql(message.model._id);
-                playlist.name.should.be.eql(message.model.title);
-                moment(playlist.start).valueOf().should.eql(message.model.start);
+                playlist.get('name').should.be.eql(message.model.title);
+                moment(playlist.get('start')).valueOf().should.eql(message.model.start);
                 done();
             });
             self.pubsub.publishJSON(message.channel(), message);
@@ -156,7 +163,8 @@ describe('PlaylistMongoDriver', function(){
         it('should return playlists', function(done) {
             self.driver.getPlaylists({from: self.from, to: self.to}, function(playlists) {
                 playlists.length.should.not.be.eql(0);
-                playlists.forEach(function(playlist) {
+                playlists.forEach(function(pl) {
+                    var playlist = pl.toJSON();
                     playlist.should.have.property('id');
                     playlist.should.have.property('name');
                     playlist.should.have.property('start');
